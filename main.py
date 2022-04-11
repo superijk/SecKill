0f954afb3c3a52e4355346d0114906febaac94fc
XXX <942451858@qq.com>
2022-04-11 10:02:21 +0800

    init

--- main.py
+++ main.py
@@ -25,16 +25,20 @@ class MainDialog(QMainWindow):
         self.timer.timeout.connect(self.showTime)
         self.waitflag = 0  # 用于标记是否在等待
         self.buylater = SetTime()
-        self.setStyleSheet(qrainbowstyle.load_stylesheet(style="DarkOrange"))
+        #self.setStyleSheet(qrainbowstyle.load_stylesheet(style="DarkOrange"))
         self.screen = QDesktopWidget().screenGeometry()
-        size = self.geometry()
-        self.move((self.screen.width() - size.width() - 80), (self.screen.height() - size.height()) / 2)
-        self.browser = webdriver.Chrome()
+        #size = self.geometry()
+        #self.move((self.screen.width() - size.width() - 80), (self.screen.height() - size.height()) / 2)
+        option = webdriver.ChromeOptions()
+        option.add_experimental_option('excludeSwitches', ['enable-automation'])  # 这里去掉window.navigator.webdriver的特性
+        option.add_argument("--disable-blink-features=AutomationControlled")
+        self.browser = webdriver.Chrome(options = option)
+        #self.browser = webdriver.Chrome()
         self.browser.set_window_size(self.screen.width() / 2, self.screen.height())
         print(sys.path[0])
-        self.manual = "file:///" + sys.path[0] + "/manual/index.html"
-        self.about = "file:///" + sys.path[0] + "/manual/about.html"
-        self.browser.get(self.manual)
+        #self.manual = "file:///" + sys.path[0] + "/manual/index.html"
+        #self.about = "file:///" + sys.path[0] + "/manual/about.html"
+        #self.browser.get(self.manual)
         self.ui = my_ui.SeckillUi.SeckillUi()
         self.ui.setupUi(self)
         self.ui.btn_now.clicked.connect(self.buy_it_now)
@@ -70,21 +74,72 @@ class MainDialog(QMainWindow):
             self.ui.textBrowser.append("请勿关闭浏览器！抢单失败！")
             return -1
         goods_url = self.ui.lineEdit.text()
+        text = self.browser.find_element(By.XPATH, "//*[@id='J_SiteNavLogin']/div[1]/div/a").text
+        noLogin = True
+        if "亲，请登录" != text:
+            noLogin = False
         try:
-            loc = (By.LINK_TEXT, "亲，请登录")
-            WebDriverWait(self.browser, 30, 0.5).until(EC.visibility_of_element_located(loc))
-            self.browser.find_element(By.LINK_TEXT, "亲，请登录").click()
+            text = self.browser.find_element(By.XPATH, "//*[@id='J_SiteNavLogin']/div[1]/div/a").text
+            if noLogin:
+                loc = (By.LINK_TEXT, "亲，请登录")
+                WebDriverWait(self.browser, 30, 10).until(EC.visibility_of_element_located(loc))
+                self.browser.find_element(By.LINK_TEXT, "亲，请登录").click()
+
         except:
             self.ui.textBrowser.append("登录失败！")
             return -1
 
         try:
-            loc = (By.XPATH, "//*[@id='login']/div[1]/i")
-            WebDriverWait(self.browser, 30, 0.5).until(EC.visibility_of_element_located(loc))
-            self.browser.find_element(By.XPATH, "//*[@id='login']/div[1]/i").click()  # 淘宝切换二维码登录按钮
-            self.ui.textBrowser.append("请使用手机淘宝扫码登录")
-            self.ui.textBrowser.moveCursor(self.ui.textBrowser.textCursor().End)
-            WebDriverWait(self.browser, 30, 0.5).until_not(EC.visibility_of_element_located(loc))
+            if noLogin:
+                # 二维码登录
+                loc = (By.XPATH, "//*[@id='login']/div[1]/i")
+                WebDriverWait(self.browser, 30, 0.5).until(EC.visibility_of_element_located(loc))
+                self.browser.find_element(By.XPATH, "//*[@id='login']/div[1]/i").click()  # 淘宝切换二维码登录按钮
+                self.ui.textBrowser.append("请使用手机淘宝扫码登录")
+                self.ui.textBrowser.moveCursor(self.ui.textBrowser.textCursor().End)
+                WebDriverWait(self.browser, 30, 0.5).until_not(EC.visibility_of_element_located(loc))
+
+                # 短信登录
+                # loc = (By.XPATH, "//*[@id='login']/div[2]/div/div[1]/a[2]")
+                # WebDriverWait(self.browser, 30, 0.5).until(EC.visibility_of_element_located(loc))
+                # self.browser.find_element(By.XPATH, "//*[@id='login']/div[2]/div/div[1]/a[2]").click()  # 淘宝切换短信登录按钮
+                # self.ui.textBrowser.append("请使用手机验证码登录")
+                # self.ui.textBrowser.moveCursor(self.ui.textBrowser.textCursor().End)
+                #
+                #
+                # #手机号
+                # loc = (By.XPATH,"//*[@id='fm-sms-login-id']")
+                # WebDriverWait(self.browser, 30, 0.5).until(EC.visibility_of_element_located(loc))
+                # phone = "15989306469"
+                # self.browser.find_element(By.XPATH, "//*[@id='fm-sms-login-id']").send_keys(phone)
+                #
+                # #判断滑块是否存在
+                # try:
+                #     # 找到滑块
+                #     slider = self.browser.find_element(By.XPATH,"//span[contains(@class, 'btn_slide')]")
+                #     loc = (By.XPATH, "//span[contains(@class, 'btn_slide')]")
+                #     # 判断滑块是否可见
+                #     if slider.is_displayed():
+                #         ActionChains(self.browser)
+                #         # 点击并且不松开鼠标
+                #         ActionChains(self.browser).click_and_hold(on_element=slider).perform()
+                #         # 往右边移动258个位置
+                #         ActionChains(self.browser).move_by_offset(xoffset=258, yoffset=0).perform()
+                #         # 松开鼠标
+                #         ActionChains(self.browser).pause(0.5).release().perform()
+                # except:
+                #     pass
+                # # button = WebDriverWait(self.browser, timeout=30).until(
+                # #     EC.presence_of_element_located((By.CLASS_NAME, 'password-login')))
+                # # button.click()
+                #
+                # #获取验证码
+                # loc = (By.XPATH, "//*[@id='login-form']/div[2]/div[3]/a")
+                # WebDriverWait(self.browser, 30, 0.5).until(EC.visibility_of_element_located(loc))
+                # self.browser.find_element(By.XPATH, "//*[@id='login-form']/div[2]/div[3]/a").click()
+                # WebDriverWait(self.browser, 30, 0.5).until_not(EC.visibility_of_element_located(loc))
+
+
         except:
             self.ui.textBrowser.append("等待扫码时间过长，登陆失败！")
             return -1
@@ -97,13 +152,29 @@ class MainDialog(QMainWindow):
             self.ui.textBrowser.append("进入商品页面失败，请检查链接能否正常访问")
             return -1
 
+        # try:
+        #     # 鞋码
+        #     size = "43"
+        #     loc = (By.XPATH, "//span[text()='" + size + "']")
+        #     WebDriverWait(self.browser, 30, 0.5).until(EC.visibility_of_element_located(loc))
+        #     self.browser.find_element(By.XPATH, "//span[text()='" + size + "']").click()
+        #
+        #     # 颜色分类
+        #     type = "水蓝色/荧光粉绿"
+        #     loc = (By.XPATH, "//span[text()='" + type + "']")
+        #     WebDriverWait(self.browser, 30, 0.5).until(EC.visibility_of_element_located(loc))
+        #     self.browser.find_element(By.XPATH, "//span[text()='" + type + "']").click()
+        # except Exception as err:
+        #     self.ui.textBrowser.append("选择鞋码和颜色分类失败，请尝试手动选择")
+
         # 点击购买按钮
         while True:
             try:
                 self.ui.textBrowser.append("请选择商品详情，等待开抢...")
-                loc = (By.XPATH, "//*[@id=\"J_juValid\"]/div[1]/a")
+                #loc = (By.XPATH, "//*[@id=\"J_juValid\"]/div[1]/a")
+                loc = (By.LINK_TEXT, '立即购买')
                 WebDriverWait(self.browser, 30, 0.01).until(EC.visibility_of_element_located(loc))
-                self.browser.find_element(By.XPATH, "//*[@id=\"J_juValid\"]/div[1]/a").click()
+                self.browser.find_element(By.LINK_TEXT, '立即购买').click()
                 break
             except:
                 self.ui.textBrowser.append("正在等待开抢/抢购中...")
@@ -209,7 +280,8 @@ class MainDialog(QMainWindow):
             self.ui.textBrowser.setText("开始淘宝抢单...")
             self.ui.btn_now.setDisabled(True)
             sniffer_thread = threading.Thread(target=self.quick_buy2tao)
-            sniffer_thread.setDaemon(True)
+            #sniffer_thread.setDaemon(True)
+            sniffer_thread.daemon = True
             sniffer_thread.start()
         elif self.ui.choose_jing.isChecked():
             self.ui.textBrowser.setText("开始京东抢单...")
@@ -229,7 +301,7 @@ class MainDialog(QMainWindow):
             self.browser.get(self.manual)
         except:
             self.browser = webdriver.Chrome()
-            self.browser.get(self.manual)
+            #self.browser.get(self.manual)
         self.ui.textBrowser.setText("抢单已终止！\n请重新选择抢单...")
         self.stop_thread.set()
         self.ui.btn_now.setDisabled(False)
